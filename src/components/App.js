35d9e04d12e4a1b0db845bede5a749c5e999ca2a
RenatSayf <renatsayf@gmail.com>
2018-10-24 22:56:28 +0500

    bootstrap, jquery has been added

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <div class="container">
+            <div class="row">
+              <h1 class="col-12 text-center">
+                Ты сегодня покормил кота?
+              </h1>
+            </div>
+          </div>
         </header>
       </div>
     );
