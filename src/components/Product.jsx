3166e979cb492282f4a2666cf99b383230b8a0cf
RenatSayf <renatsayf@gmail.com>
2018-10-30 22:38:19 +0500

    ok

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class Product extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {isToggleOn: true};
+    //     this.onClick = this.onClick.bind(this);
+    // }
     render(){
         const product = this.props.product;
         console.log('----', this.props.product);
+        const selected = product.product_selected;
         return(
             <div className='product'>
-                <div className='product-img'>
+                <div id={product.id} className={['product-img', selected].join(' ')} onClick={onClick}>
                     <div>
                         Сказочное заморское явство
                     </div>
@@ -30,10 +36,15 @@ class Product extends Component{
                     </div>
                 </div>
                 <div className='product-footer'>
-                    Чего сидишь, Порадуй котэ, <a href="#">купи.</a>
+                    Чего сидишь, Порадуй котэ, <span onClick={onClick}>купи.</span>
                 </div>
             </div>
         )
+
+        function onClick(event){
+            const s = event.target.attributes.id.nodeValue;
+            debugger;
+        }
     }
 }
 
--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 export default[
     {
+        "id":"1",
         "with_what":"с фуа-гра",
         "dose":"10 порций",
         "present":"мышь в подарок",
@@ -7,9 +8,11 @@ export default[
         "weight":"0,5 кг",
         "product_footer1":"Чего сидишь? Порадуй коте, купи",
         "product_footer2":"Отличное лакомство с фуа-гра",
-        "product_footer3":"Печалька, с фуа-гра закончился."
+        "product_footer3":"Печалька, с фуа-гра закончился.",
+        "product_selected":""
     },
     {
+        "id":"2",
         "with_what":"с рыбой",
         "dose":"40 порций",
         "present":"2 мыши в подарок",
@@ -17,9 +20,11 @@ export default[
         "weight":"2 кг",
         "product_footer1":"Чего сидишь? Порадуй коте, купи",
         "product_footer2":"Головы щучьи с чесноком, да свежайшая семгушка",
-        "product_footer3":"Печалька, с рыбой закончился"
+        "product_footer3":"Печалька, с рыбой закончился",
+        "product_selected":""
     },
     {
+        "id":"3",
         "with_what":"с курой",
         "dose":"100 порций",
         "present":"5 мышей в подарок",
@@ -27,6 +32,7 @@ export default[
         "weight":"5 кг",
         "product_footer1":"Чего сидишь? Порадуй коте, купи",
         "product_footer2":"Нежная мякоть курочки",
-        "product_footer3":"Печалька, с курой закончились"
+        "product_footer3":"Печалька, с курой закончились",
+        "product_selected":"none"
     }
 ]
