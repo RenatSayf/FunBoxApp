94d602cf69422c035f959e4fda17c2ec21e88aac
RenatSayf <renatsayf@gmail.com>
2018-10-31 22:10:17 +0500

    ok

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class Product extends Component{
-    // constructor(props){
-    //     super(props);
-    //     this.state = {isToggleOn: true};
-    //     this.onClick = this.onClick.bind(this);
-    // }
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn: true};
+        this.onClick = this.onClick.bind(this);
+    }
     render(){
         const product = this.props.product;
         console.log('----', this.props.product);
         const selected = product.product_selected;
         return(
             <div className='product'>
-                <div id={product.id} className={['product-img', selected].join(' ')} onClick={onClick}>
+                <div id={product.id} className={['product-img', selected].join(' ')} onClick={this.onClick}>
                     <div>
                         Сказочное заморское явство
                     </div>
@@ -36,15 +36,15 @@ class Product extends Component{
                     </div>
                 </div>
                 <div className='product-footer'>
-                    Чего сидишь, Порадуй котэ, <span onClick={onClick}>купи.</span>
+                    Чего сидишь, Порадуй котэ, <span onClick={this.onClick}>купи.</span>
                 </div>
             </div>
-        )
+        )        
+    }
 
-        function onClick(event){
-            const s = event.target.attributes.id.nodeValue;
-            debugger;
-        }
+    onClick = (event) => {
+        const s = event.currentTarget.attributes.id;
+        debugger;
     }
 }
 
