5b4d632ee210d6c6043e802d4a8c7eba220b61c6
RenatSayf <renatsayf@gmail.com>
2018-11-06 22:38:47 +0500

    Product.jsx has been updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,10 +3,17 @@ import notSelectedBack from '../images/Back.png'
 import selectedBack from '../images/SelectedBack.png';
 import disabledBack from '../images/DisabledBack.png'
 
+const color = {
+    "NotSelected":"#00bfff",
+    "Selected":"#ff00ff",
+    "Disabled":"#808080"
+}
+
 class Product extends Component{    
     state = {
         isPresence : this.props.products.presence,
-        isSelected : this.props.products.product_selected
+        isSelected : this.props.products.product_selected,
+        isBuyReferenced : this.props.products.buy_referenced
     }
     render(){
         const products = this.props.products;
@@ -15,14 +22,30 @@ class Product extends Component{
         if (this.state.isSelected) {
             bgImage = `url(${selectedBack})`;
         }
+
+        let weightCircleColor = this.state.isPresence ? color.NotSelected : color.Disabled;
+        if (this.state.isSelected) {
+            weightCircleColor = color.Selected;
+        }
+
+        let productFooterContent = this.state.isPresence ? products.product_footer1 : products.product_footer3;
+        if (this.state.isSelected) {
+            productFooterContent = products.product_footer2;
+        }
+
+        let buyReferenced = this.state.isPresence ? " купи" : "";
+        if (this.state.isSelected) {
+            buyReferenced = "";
+        }
+
         return(
             <div id={products.id} className='product'>
                 <div className='product-img' onClick={onClickEvent} style={{backgroundImage: bgImage}}>
                     <div>
-                        Сказочное заморское явство
+                        {products.tagline}
                     </div>
                     <h1>
-                        Нямушка
+                        {products.product_name}
                     </h1>
                     <h3>
                         {products.with_what}
@@ -36,12 +59,12 @@ class Product extends Component{
                     <div>
                         {products.customer_satisfied}
                     </div>
-                    <div className='product-weight'>
+                    <div className='product-weight' style={{backgroundColor: weightCircleColor}}>
                         {products.weight}
                     </div>
                 </div>
                 <div className='product-footer'>
-                    Чего сидишь, Порадуй котэ, <span onClick={onClickEvent}>купи.</span>
+                    {productFooterContent}<span onClick={onClickEvent}>{buyReferenced}</span>
                 </div>
             </div>
         )        
--- src/data.js
+++ src/data.js
@@ -1,41 +1,50 @@
 export default[
     {
+        "tagline":"Сказочное заморское явство",
+        "product_name":"Нямушка",
         "id":"1",
         "with_what":"с фуа-гра",
         "dose":"10 порций",
         "present":"мышь в подарок",
         "customer_satisfied":" ",
         "weight":"0,5 кг",
-        "product_footer1":"Чего сидишь? Порадуй коте, купи",
+        "product_footer1":"Чего сидишь? Порадуй коте, ",
         "product_footer2":"Отличное лакомство с фуа-гра",
         "product_footer3":"Печалька, с фуа-гра закончился.",
         "product_selected":false,
-        "presence":true
+        "presence":true,
+        "buy_referenced": true
     },
     {
+        "tagline":"Сказочное заморское явство",
+        "product_name":"Нямушка",
         "id":"2",
         "with_what":"с рыбой",
         "dose":"40 порций",
         "present":"2 мыши в подарок",
         "customer_satisfied":" ",
         "weight":"2 кг",
-        "product_footer1":"Чего сидишь? Порадуй коте, купи",
+        "product_footer1":"Чего сидишь? Порадуй коте, ",
         "product_footer2":"Головы щучьи с чесноком, да свежайшая семгушка",
         "product_footer3":"Печалька, с рыбой закончился",
         "product_selected":false,
-        "presence":true
+        "presence":true,
+        "buy_referenced": true
     },
     {
+        "tagline":"Сказочное заморское явство",
+        "product_name":"Нямушка",
         "id":"3",
         "with_what":"с курой",
         "dose":"100 порций",
         "present":"5 мышей в подарок",
         "customer_satisfied":"заказчик доволен",
         "weight":"5 кг",
-        "product_footer1":"Чего сидишь? Порадуй коте, купи",
+        "product_footer1":"Чего сидишь? Порадуй коте, ",
         "product_footer2":"Нежная мякоть курочки",
         "product_footer3":"Печалька, с курой закончились",
         "product_selected":false,
-        "presence":false
+        "presence":false,
+        "buy_referenced": true
     }
 ]
