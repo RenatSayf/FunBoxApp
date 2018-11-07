a5c40910597477fe456c7909bd23755f4de6d9b2
RenatSayf <renatsayf@gmail.com>
2018-11-07 22:35:22 +0500

    Product.jsx, App.scss and other has been updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,12 +3,6 @@ import notSelectedBack from '../images/Back.png'
 import selectedBack from '../images/SelectedBack.png';
 import disabledBack from '../images/DisabledBack.png'
 
-const color = {
-    "NotSelected":"#00bfff",
-    "Selected":"#ff00ff",
-    "Disabled":"#808080"
-}
-
 class Product extends Component{    
     state = {
         isPresence : this.props.products.presence,
@@ -16,54 +10,66 @@ class Product extends Component{
         isBuyReferenced : this.props.products.buy_referenced
     }
     render(){
+        let productClass = 'product-non-selected';
+        let productImgClass = 'product-img-non-selected';
+        let productTagLineClass = 'product-tagline-non-selected';
+        let productNameClass = 'product-name-non-selected';
+        let productWithWhatClass = 'product-with-what-non-selected';
+        let productDoseClass = 'product-dose-non-selected';
+        let productPresentClass = 'product-present-non-selected';
+        let productCustomerSatisfiedClass = 'product-customer-satisfied-non-selected';
+        let productWeightClass = 'product-weight-non-selected';
+        let productFooterClass = 'product-footer-non-selected';
+        
+
         const products = this.props.products;
         const onClickEvent = this.state.isPresence ? this.onClick : null;
-        let bgImage = this.state.isPresence ? `url(${notSelectedBack})` : `url(${disabledBack})`;
-        if (this.state.isSelected) {
-            bgImage = `url(${selectedBack})`;
-        }
-
-        let weightCircleColor = this.state.isPresence ? color.NotSelected : color.Disabled;
-        if (this.state.isSelected) {
-            weightCircleColor = color.Selected;
-        }
-
-        let productFooterContent = this.state.isPresence ? products.product_footer1 : products.product_footer3;
-        if (this.state.isSelected) {
-            productFooterContent = products.product_footer2;
-        }
-
         let buyReferenced = this.state.isPresence ? " купи" : "";
+        let productFooterContent = products.product_footer1;
+
+        if (!this.state.isPresence) {
+            productClass = 'product-is-disabled';
+            productImgClass = 'product-img-is-disabled';
+            productWeightClass = 'product-weight-is-disabled';
+            productFooterClass = 'product-footer-is-disabled';
+            productFooterContent = products.product_footer3;
+        }
+        
         if (this.state.isSelected) {
+            productClass = 'product-is-selected';
+            productImgClass = 'product-img-is-selected';
+            productWeightClass = 'product-weight-is-selected';
+            productFooterClass = 'product-footer-is-selected';
+            productFooterContent = products.product_footer2;
             buyReferenced = "";
         }
-
+        
         return(
-            <div id={products.id} className='product'>
-                <div className='product-img' onClick={onClickEvent} style={{backgroundImage: bgImage}}>
-                    <div>
+            <div id={products.id} className={productClass}>
+                <div className={productImgClass} onClick={onClickEvent}>
+                    <div className={productTagLineClass}>
                         {products.tagline}
                     </div>
-                    <h1>
+                    <div className={productNameClass}>
                         {products.product_name}
-                    </h1>
-                    <h3>
+                    </div>
+                    <div className={productWithWhatClass}>
                         {products.with_what}
-                    </h3>
-                    <div>
+                    </div>
+                    <div className={productDoseClass}>
                         {products.dose}
                     </div>
-                    <div>
+                    <div className={productPresentClass}>
                         {products.present}
                     </div>
-                    <div>
+                    <div className={productCustomerSatisfiedClass}>
                         {products.customer_satisfied}
                     </div>
-                    <div className='product-weight' style={{backgroundColor: weightCircleColor}}>
+                    <div className={[productWeightClass, 'product-weight-cyrcle'].join(' ')}>
                         {products.weight}
                     </div>
                 </div>
-                <div className='product-footer'>
+                <div className={productFooterClass}>
                     {productFooterContent}<span onClick={onClickEvent}>{buyReferenced}</span>
                 </div>
             </div>
