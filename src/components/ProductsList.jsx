c29179dc936126be6fff6b95ea58b2dd43c1048e
RenatSayf <renatsayf@gmail.com>
2018-11-11 20:34:02 +0500

    ProductList.jsx has been updated

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Product from './Product'
 
-export default function ProductsList({products}) {
-    const productsItems = products.map((product) => {
-        return (
-        <div key={product.id} className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 text-center">
-            <Product products = {product}/>
-        </div>)
+class ProductsList extends Component {
+    state = {
+        windowWidth: window.innerWidth
+      }    
+
+    render() {
+        const screen_width = this.state.windowWidth;
+        const isOdd = this.props.products.length % 2;
+        let class_name = "col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 text-center";
+        const productsItems = this.props.products.map((product, index) => {
+            if (index === this.props.products.length - 1 && isOdd === 1 && (screen_width > 760 && screen_width < 1030)) {
+                class_name = "col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 offset-md-3 text-center";
+            }
+            return (
+                <div key={product.id} className={class_name}>
+                    <Product products={product} />
+                </div>
+            )
         });
 
-    return(        
+        return (
             productsItems
-    )
+        )
+    }
+
+    handleResize = () => {
+        this.setState({
+            windowWidth: window.innerWidth
+        })
+    }
+
+    componentDidMount() {
+        this.handleResize();
+        window.addEventListener('resize', this.handleResize.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize.bind(this));
+    }
 }
+
+export default ProductsList;
